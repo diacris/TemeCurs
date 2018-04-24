cf3b18cbe9757e9e90b44095b47fdb17c47c30a8
diacris <38329014+diacris@users.noreply.github.com>
2018-04-25 00:05:28 +0300

    Tema1

---
+++ Tema1/Tema1/Laptop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1
+{
+    class Laptop // declara clasa Laptop
+    {
+        public string culoare = "gri"; // declara atributul culoare si am atribuit valoarea gri
+        public string sistemDeOperare = "windows";
+        public int baterie = 100;
+        public int lungime = 38;
+        public int latime = 25;
+
+        public void AfiseazaAtribute()
+        {
+            Console.WriteLine($"Culoare: {culoare}, Sistem de operare: {sistemDeOperare}, Baterie {baterie}, Diagonala {Diagonala()}," +
+                $" Lungime {lungime}, Latime {latime}");
+
+        }
+
+        public int ModificaLungime(int lungimenoua)
+        {
+            lungime = lungime + lungimenoua;
+
+            return lungime;
+        }
+
+        public double CalculDiagonala(int a, int b)
+        {
+            var ipotenuzaLaPatrat = a * a + b * b;
+            return Math.Sqrt(ipotenuzaLaPatrat); // radical 
+        }
+
+        public double Diagonala()
+        {
+            return CalculDiagonala(lungime, latime);
+        }
+
+        public void DescarcaBaterie(int x)
+        {
+            baterie = baterie - x;
+        }
+
+        public void IncarcaBaterie(int y)
+        {
+            baterie = baterie + y;
+        }
+
+    }
+}
+
+
+
---
+++ Tema1/Tema1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1
+{
+    class Program // declarare clasa
+    {
+        static void Main(string[] args) // declarare metoda main
+        {
+            Laptop laptop = new Laptop();  // instantiere clasa laptop in variabila laptop, rezultatul instantierii este un obiect
+            laptop.AfiseazaAtribute();  
+            laptop.culoare = "negru";
+            laptop.DescarcaBaterie(30); 
+            laptop.AfiseazaAtribute();
+
+            Console.WriteLine(laptop.ModificaLungime(1));
+
+            int rezultat = laptop.ModificaLungime(1) + laptop.ModificaLungime(2);
+
+            laptop.IncarcaBaterie(28); 
+            laptop.AfiseazaAtribute();
+
+            Console.WriteLine(rezultat);
+
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
+
+
+
+
+    
